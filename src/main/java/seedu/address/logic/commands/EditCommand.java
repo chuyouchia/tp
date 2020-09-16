b401c7b4bfc399f7fa4505d134a243a28a0a2ade
chuyouchia <58682208+chuyouchia@users.noreply.github.com>
2020-09-16 14:29:34 +0800

    Replace the error message with the actual logic to modify the remarks of a person.
    Update RemarkCommandTest to test that execute() logic works.
    
    Connect Remark field to PersonCard.
    
    Add Person#getRemark()
    
    Modify PersonBuilder to support Remark.
    
    Modify AddCommand to create a Person with a blank Remark.
    
    Modify EditCommand to pass Remarks on unchanged.
    
    Modify SampleDataUtil to add remarks for the sample data.
    
    Add withRemark() to PersonBuilder.
    
    Modify RemarkCommand to take in an Index and String for remark, and
    print those two parameters as the error message
    
    Modify RemarkCommandTest to test the equals method
    
    Add RemarkCommandParser that will know how to parse two arguments, one
    index and one with prefix 'r/'
    
    Add RemarkCommandParserTest that will test different boundary values
    for RemarkCommandParser.
    
    Modify AddressBookParser to use the newly implemented
    RemarkCommandParser
    
    Modify AddressBookParserTest to ensure that what the user input
    generated the correct command.

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -5,6 +5,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
@@ -24,6 +25,7 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Remark;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -42,6 +44,7 @@ public class EditCommand extends Command {
             + "[" + PREFIX_EMAIL + "EMAIL] "
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_TAG + "TAG]...\n"
+            + "[" + PREFIX_REMARK + "REMARK]...\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_PHONE + "91234567 "
             + PREFIX_EMAIL + "johndoe@example.com";
@@ -98,8 +101,9 @@ public class EditCommand extends Command {
         Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
         Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
         Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
+        Remark remark = personToEdit.getRemark(); //format does not parse for edits in remarks yet
 
-        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedTags);
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, remark, updatedTags);
     }
 
     @Override
